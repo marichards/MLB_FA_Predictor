a2ed3f4cbb9022dab369bd5874ac5f074d33a290
marichards <marichards88@gmail.com>
2018-01-25 10:10:36 -0800

    Changed to AAV

--- models.py
+++ models.py
@@ -19,11 +19,11 @@ def loadAllData():
     '''Load both saved data frames for use in the models'''
 
     # Load pitchers
-    with open('pitching_data.pickle', 'rb') as file:
+    with open('/home/ubuntu/Github/MLB_FA_Predictor/mlb_flask_app/pitching_data.pickle', 'rb') as file:
         pitcher_all = pickle.load(file)
 
     # Load position players
-    with open('position_data.pickle', 'rb') as file:
+    with open('/home/ubuntu/Github/MLB_FA_Predictor/mlb_flask_app/position_data.pickle', 'rb') as file:
         position_all = pickle.load(file)
 
     return pitcher_all, position_all
@@ -37,6 +37,9 @@ def prepareFreeAgentData(df):
 
     # Create Contract logical variable
     df['Contract'] = df.Dollars.notnull()
+
+    # Create AAV variable
+    df['AAV_2006'] = df.Dollars_2006.divide(df.Length)
 
     return df
 
@@ -59,8 +62,8 @@ def splitDataByYear(df, year, player_type):
     # Create the 4 sets of data; X's are arrays, y's are DFs for now
     X_train = df_train[features].values
     X_test = df_test[features].values
-    y_train = df_train[['Dollars_2006','Length','Contract']]
-    y_test = df_test[['Dollars_2006','Length','Contract', 'Dollars']]
+    y_train = df_train[['AAV_2006','Length','Contract']]
+    y_test = df_test[['AAV_2006','Length','Contract', 'Dollars']]
 
     return X_train, y_train, X_test, y_test    
 
@@ -84,7 +87,7 @@ def predictContract(X_train, y_train, X_test):
 def predictLength(X_train, y_train, X_test):
 
     # Shorten training to only non-null dollars
-    idx = y_train.Dollars_2006.notnull()
+    idx = y_train.AAV_2006.notnull()
     y_train = y_train[idx]
     X_train = X_train[idx]
 
@@ -105,12 +108,12 @@ def predictLength(X_train, y_train, X_test):
 def predictDollars(X_train, y_train, X_test):
 
     # Shorten training to only non-null dollars
-    idx = y_train.Dollars_2006.notnull()
+    idx = y_train.AAV_2006.notnull()
     y_train = y_train[idx]
     X_train = X_train[idx]
 
     # Grab the y values
-    y_train_values = y_train['Dollars_2006'].values
+    y_train_values = y_train['AAV_2006'].values
     
     # Designate a logistic regression model
     gbr = GradientBoostingRegressor()
